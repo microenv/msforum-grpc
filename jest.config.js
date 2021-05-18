5b9a600007478184b142c8ccd2c3b992a317c5a9
Giovanne Afonso <giovanneafonso@gmail.com>
2021-05-18 15:12:27 -0300

    Setup jest config

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  moduleDirectories: [
+    "node_modules",
+    ".",
+  ],
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "ts",
+  ],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  collectCoverageFrom: [
+    "**/*.(t|j)s",
+  ],
+  coverageDirectory: "../coverage",
+  coveragePathIgnorePatterns: [
+    ".d.ts",
+    "main.ts",
+  ],
+  testEnvironment: "node",
+  coverageThreshold: {
+    global: {
+      "statements": 90,
+      "branches": 90,
+      "functions": 90,
+      "lines": 90,
+    }
+  }
+}
