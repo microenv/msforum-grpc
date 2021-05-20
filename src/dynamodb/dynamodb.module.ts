cc318c787ef642c17b5d8b199f90f92b2a5a2b84
Giovanne Afonso <giovanneafonso@gmail.com>
2021-05-19 23:16:07 -0300

    Setup docker compose

--- src/dynamodb/dynamodb.module.ts
+++ src/dynamodb/dynamodb.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from 'src/config/config.module';
 import { DynamodbService } from './dynamodb.service';
 
+@Global()
 @Module({
   imports: [ConfigModule],
   providers: [DynamodbService],
